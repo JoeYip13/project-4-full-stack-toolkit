9e68d5a7e83a6471b846bd83be793055f021b297
JoeYip13 <>
2023-08-01 07:56:57 +0000

    Deployment commit for Django Admin panel

--- tasty_tales/settings.py
+++ tasty_tales/settings.py
@@ -28,7 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['tasty-tales-by-joe-yip-e31af4ace719.herokuapp.com', '8000-joeyip13-project-4-full-8tbf0cys1r.us2.codeanyapp.com']
 
