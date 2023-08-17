97ba3fd1c587c4614906719a4470735936dd3856
JoeYip13 <>
2023-08-17 17:17:53 +0000

    Add contact form and view function

--- about/views.py
+++ about/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import About
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from .models import About, Contact, ContactMessage
+from .forms import ContactForm
 
 
 def about_me(request, *args, **kwargs):
@@ -15,3 +18,38 @@ def about_me(request, *args, **kwargs):
             "about": about
         },
     )
+
+
+def contact_page_view(request, *args, **kwargs):
+    """
+    Renders the Contact page
+    """
+    submitted = False
+    contact = Contact.objects.first()
+    contact_messages = ContactMessage.objects.all()
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent!')
+            HttpResponseRedirect('/contact?submitted=True')
+    else:
+        form = ContactForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    form = ContactForm()
+
+    context = {
+        'contact': contact,
+        'contact_messages': contact_messages,
+        'form': form,
+    }
+
+    return render(
+        request,
+        'contact_page.html',
+        context,
+    )
+
