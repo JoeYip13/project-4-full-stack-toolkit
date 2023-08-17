26c09761d1a91edae55fbf95afe6f94e4ca6a91f
JoeYip13 <>
2023-08-17 17:22:52 +0000

    Add contact to admin

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import About
+from .models import About, Contact, ContactMessage
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -9,3 +9,18 @@ class AboutAdmin(SummernoteModelAdmin):
     list_display = ('title',)
     search_fields = ['title', 'content']
     summernote_fields = ('content',)
+
+
+@admin.register(Contact)
+class ContactAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'email', 'contact_number')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content',)
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'email', 'subject', 'message', 'timestamp')
+    search_fields = ['name', 'timestamp']
