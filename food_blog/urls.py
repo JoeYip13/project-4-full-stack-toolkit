006c0db0b845745fe0e538c49552e483cda1d93e
JoeYip13 <>
2023-08-22 10:32:31 +0000

    Add update post url

--- food_blog/urls.py
+++ food_blog/urls.py
@@ -5,6 +5,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('add_post/', views.AddPostView.as_view(), name='add_post'),
+    path('<slug:slug>/update_post/', views.UpdatePostView.as_view(),
+        name='update_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
 ]
