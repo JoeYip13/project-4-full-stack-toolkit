48096094795de1d9326d2f1c823e92325d2e254e
JoeYip13 <>
2023-08-22 16:03:49 +0000

    Add delete view url

--- food_blog/urls.py
+++ food_blog/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('add_post/', views.AddPostView.as_view(), name='add_post'),
     path('<slug:slug>/update_post/', views.UpdatePostView.as_view(),
-        name='update_post'),
+         name='update_post'),
+    path('<slug:slug>/delete_post/', views.DeletePostView.as_view(),
+         name='delete_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
 ]
