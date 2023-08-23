ebbddb53e394f15cf8fdfa0639811192264b89a8
JoeYip13 <>
2023-08-23 13:49:50 +0000

    Add urls for comment edit and delete

--- food_blog/urls.py
+++ food_blog/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
          name='delete_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
